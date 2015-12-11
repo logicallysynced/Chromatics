1f0be8f03aabcec19d2ef79c177ed3f25eaee24f
Roxas Keyheart <technicaldaniel@gmail.com>
2015-12-11 18:02:53 +1030

    Adding Chromatics Source

---
+++ Chromatics Updater/Updater_Form.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Chromatics_Updater
+{
+    partial class Updater_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Updater_Form));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.prog_Download = new System.Windows.Forms.ProgressBar();
+            this.lbl_data = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(219, 87);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // prog_Download
+            // 
+            this.prog_Download.Location = new System.Drawing.Point(12, 133);
+            this.prog_Download.Name = "prog_Download";
+            this.prog_Download.Size = new System.Drawing.Size(431, 23);
+            this.prog_Download.TabIndex = 1;
+            this.prog_Download.Click += new System.EventHandler(this.prog_Download_Click);
+            // 
+            // lbl_data
+            // 
+            this.lbl_data.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_data.Location = new System.Drawing.Point(12, 161);
+            this.lbl_data.Name = "lbl_data";
+            this.lbl_data.Size = new System.Drawing.Size(431, 32);
+            this.lbl_data.TabIndex = 2;
+            this.lbl_data.Text = "Preparing to Update Chromatics";
+            this.lbl_data.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Updater_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 216);
+            this.Controls.Add(this.lbl_data);
+            this.Controls.Add(this.prog_Download);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Updater_Form";
+            this.Text = "Updating Chromatics";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar prog_Download;
+        private System.Windows.Forms.Label lbl_data;
+    }
+}
+
---
+++ Chromatics Updater/Updater_Form.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chromatics_Updater
+{
+    public partial class Updater_Form : Form
+    {
+        public Updater_Form()
+        {
+            InitializeComponent();
+            ExecuteUpdate();
+        }
+
+        void ExecuteUpdate()
+        {
+            string[] args = Environment.GetCommandLineArgs();
+            string updatedFile = args[1];
+
+            using (var client = new WebClient())
+            {
+                client.Headers.Add("User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0)");
+                client.DownloadFileCompleted += new AsyncCompletedEventHandler((sender, e) => Completed(sender, e, updatedFile));
+                client.DownloadProgressChanged += new DownloadProgressChangedEventHandler((sender, e) => ProgressChanged(sender, e));
+                client.DownloadFileAsync(new Uri("http://thejourneynetwork.net/chromatics/update/Chromatics.zip"), updatedFile + @"\Chromatics.zip");
+            }
+
+            
+        }
+
+
+        private void prog_Download_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Completed(object sender, AsyncCompletedEventArgs e, string updatedFile)
+        {
+            FileInfo destFile = new FileInfo(Path.Combine(updatedFile, @"\Chromatics.zip"));
+
+            if (destFile.Extension.ToLower() == ".zip")
+            {
+                lbl_data.Text = "Extracting Update..";
+
+                ZipArchive zipArchive = ZipFile.OpenRead(updatedFile + @"\Chromatics.zip");
+
+                foreach (ZipArchiveEntry entry in zipArchive.Entries)
+                {
+                    string fullPath = Path.Combine(updatedFile + @"\", entry.FullName);
+                    if (String.IsNullOrEmpty(entry.Name))
+                    {
+                        Directory.CreateDirectory(fullPath);
+                    }
+                    else
+                    {
+                        if (!entry.Name.Equals("updater.exe"))
+                        {
+                            entry.ExtractToFile(fullPath, true);
+                        }
+                    }
+                }
+                
+                zipArchive.Dispose();
+                //File.Delete(updatedFile + @"\Chromatics.zip");
+                lbl_data.Text = "Closing Updater";
+                if (File.Exists(@"C:\Program Files (x86)\Advanced Combat Tracker\Advanced Combat Tracker.exe"))
+                {
+                    ProcessStartInfo startInfo = new ProcessStartInfo();
+                    startInfo.FileName = @"C:\Program Files (x86)\Advanced Combat Tracker\Advanced Combat Tracker.exe";
+                    Process.Start(startInfo);
+                }
+                System.Windows.Forms.Application.Exit();
+            }
+            else
+            {
+                lbl_data.Text = "The downloaded file did not contain a valid Chromatics file (Unknown file type)";
+            }
+
+
+        }
+
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            prog_Download.Value = e.ProgressPercentage;
+            lbl_data.Text = "Downloading Update: " + e.ProgressPercentage + "% Complete";
+        }
+    }
+
+}
