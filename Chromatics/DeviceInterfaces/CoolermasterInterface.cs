c4535c7f52b1cc02a2a8a97319387315b1aff815
Roxas Keyheart <technicaldaniel@gmail.com>
2017-07-13 16:44:45 +0930

    Added new device interface templates

---
+++ Chromatics/DeviceInterfaces/CoolermasterInterface.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chromatics.DeviceInterfaces
+{
+    class CoolermasterInterface
+    {
+        public static CoolermasterLib InitializeCoolermasterSDK()
+        {
+            CoolermasterLib coolermaster = null;
+            coolermaster = new CoolermasterLib();
+
+            var coolermasterstat = coolermaster.InitializeSDK();
+
+            if (!coolermasterstat)
+                return null;
+
+            return coolermaster;
+        }
+    }
+
+    public class CoolermasterSdkWrapper
+    {
+        //
+    }
+
+    public interface ICoolermasterSdk
+    {
+        bool InitializeSDK();
+    }
+
+    public class CoolermasterLib : ICoolermasterSdk
+    {
+
+        public bool InitializeSDK()
+        {
+            return true;
+        }
+    }
+}
---
+++ Chromatics/DeviceInterfaces/RoccatInterface.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chromatics.DeviceInterfaces
+{
+    class RoccatInterface
+    {
+        public static RoccatLib InitializeRoccatSDK()
+        {
+            RoccatLib roccat = null;
+            roccat = new RoccatLib();
+
+            var roccatstat = roccat.InitializeSDK();
+
+            if (!roccatstat)
+                return null;
+
+            return roccat;
+        }
+    }
+
+    public class RoccatSdkWrapper
+    {
+        //
+    }
+
+    public interface IRoccatSdk
+    {
+        bool InitializeSDK();
+    }
+
+    public class RoccatLib : IRoccatSdk
+    {
+
+        public bool InitializeSDK()
+        {
+            return true;
+        }
+    }
+}
