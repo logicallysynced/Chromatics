bbc81b5e9aad6824399abfd9160b95b1865ddd32
Liam Dawson <liam@ldaws.com>
2018-03-03 16:57:18 +1100

    use the color matrix update style for Coolermaster devices

--- Chromatics/DeviceInterfaces/CoolermasterInterface.cs
+++ Chromatics/DeviceInterfaces/CoolermasterInterface.cs
@@ -190,6 +190,285 @@ namespace Chromatics.DeviceInterfaces
 
     public class CoolermasterLib : ICoolermasterSdk
     {
+        private readonly object _initLock = new object();
+        private static readonly ILogWrite Write = SimpleIoc.Default.GetInstance<ILogWrite>();
+        private readonly List<CoolermasterSdkWrapper.DeviceIndex> _keyboards = new List<CoolermasterSdkWrapper.DeviceIndex>();
+        private readonly CoolermasterSdkWrapper.ColorMatrix _colorMatrix = new CoolermasterSdkWrapper.ColorMatrix();
+        private readonly Timer _updateTimer;
+
+        public bool IsInitialized { get; set; } = false;
+
+        #region Key Mappings
+
+        public static readonly Dictionary<string, int[]> KeyMappings = new Dictionary<string, int[]>
+        {
+            {"Escape", new[] {0, 0}},
+            {"F1", new[] {0, 1}},
+            {"F2", new[] {0, 2}},
+            {"F3", new[] {0, 3}},
+            {"F4", new[] {0, 4}},
+            {"F5", new[] {0, 6}},
+            {"F6", new[] {0, 7}},
+            {"F7", new[] {0, 8}},
+            {"F8", new[] {0, 9}},
+            {"F9", new[] {0, 11}},
+            {"F10", new[] {0, 12}},
+            {"F11", new[] {0, 13}},
+            {"F12", new[] {0, 14}},
+            {"PrintScreen", new[] {0, 15}},
+            {"Scroll", new[] {0, 16}},
+            {"Pause", new[] {0, 17}},
+            {"Macro1", new[] {0, 18}},
+            {"Macro2", new[] {0, 19}},
+            {"Macro3", new[] {0, 20}},
+            {"Macro4", new[] {0, 21}},
+            {"OmeTilde", new[] {1, 0}},
+            {"D1", new[] {1, 1}},
+            {"D2", new[] {1, 2}},
+            {"D3", new[] {1, 3}},
+            {"D4", new[] {1, 4}},
+            {"D5", new[] {1, 5}},
+            {"D6", new[] {1, 6}},
+            {"D7", new[] {1, 7}},
+            {"D8", new[] {1, 8}},
+            {"D9", new[] {1, 9}},
+            {"D0", new[] {1, 10}},
+            {"OemMinus", new[] {1, 11}},
+            {"OemEquals", new[] {1, 12}},
+            {"Backspace", new[] {1, 14}},
+            {"Insert", new[] {1, 15}},
+            {"Home", new[] {1, 16}},
+            {"PageUp", new[] {1, 17}},
+            {"NumLock", new[] {1, 18}},
+            {"NumDivide", new[] {1, 19}},
+            {"NumMultiply", new[] {1, 20}},
+            {"NumSubtract", new[] {1, 21}},
+            {"Tab", new[] {2, 0}},
+            {"Q", new[] {2, 1}},
+            {"W", new[] {2, 2}},
+            {"E", new[] {2, 3}},
+            {"R", new[] {2, 4}},
+            {"T", new[] {2, 5}},
+            {"Y", new[] {2, 6}},
+            {"U", new[] {2, 7}},
+            {"I", new[] {2, 8}},
+            {"O", new[] {2, 9}},
+            {"P", new[] {2, 10}},
+            {"OemLeftBracket", new[] {2, 11}},
+            {"OemRightBracket", new[] {2, 12}},
+            {"OemBackslash", new[] {2, 14}},
+            {"Delete", new[] {2, 15}},
+            {"End", new[] {2, 16}},
+            {"PageDown", new[] {2, 17}},
+            {"Num7", new[] {2, 18}},
+            {"Num8", new[] {2, 19}},
+            {"Num9", new[] {2, 20}},
+            {"NumAdd", new[] {2, 21}},
+            {"CapsLock", new[] {3, 0}},
+            {"A", new[] {3, 1}},
+            {"S", new[] {3, 2}},
+            {"D", new[] {3, 3}},
+            {"F", new[] {3, 4}},
+            {"G", new[] {3, 5}},
+            {"H", new[] {3, 6}},
+            {"J", new[] {3, 7}},
+            {"K", new[] {3, 8}},
+            {"L", new[] {3, 9}},
+            {"OemSemicolon", new[] {3, 10}},
+            {"OemApostrophe", new[] {3, 11}},
+            {"JpnYen", new[] {3, 12}},
+            {"Enter", new[] {3, 14}},
+            {"Num4", new[] {3, 18}},
+            {"Num5", new[] {3, 19}},
+            {"Num6", new[] {3, 20}},
+            {"LeftShift", new[] {4, 0}},
+            {"EurPound", new[] {4, 1}},
+            {"Z", new[] {4, 2}},
+            {"X", new[] {4, 3}},
+            {"C", new[] {4, 4}},
+            {"V", new[] {4, 5}},
+            {"B", new[] {4, 6}},
+            {"N", new[] {4, 7}},
+            {"M", new[] {4, 8}},
+            {"OemComma", new[] {4, 9}},
+            {"OemPeriod", new[] {4, 10}},
+            {"OemSlash", new[] {4, 11}},
+            {"RightShift", new[] {4, 14}},
+            {"Up", new[] {4, 16}},
+            {"Num1", new[] {4, 18}},
+            {"Num2", new[] {4, 19}},
+            {"Num3", new[] {4, 20}},
+            {"NumEnter", new[] {4, 21}},
+            {"LeftControl", new[] {5, 0}},
+            {"LeftWindows", new[] {5, 1}},
+            {"LeftAlt", new[] {5, 2}},
+            {"Space", new[] {5, 6}},
+            {"RightAlt", new[] {5, 10}},
+            {"RightWindows", new[] {5, 11}},
+            {"RightMenu", new[] {5, 12}},
+            {"RightControl", new[] {5, 14}},
+            {"Left", new[] {5, 15}},
+            {"Down", new[] {5, 16}},
+            {"Right", new[] {5, 17}},
+            {"Num0", new[] {5, 18}},
+            {"NumDecimal", new[] {5, 20}}
+        };
+
+        #endregion
+
+        public static IEnumerable<CoolermasterSdkWrapper.DeviceIndex> SupportedDevices => Enum.GetValues(typeof(CoolermasterSdkWrapper.DeviceIndex)).Cast<CoolermasterSdkWrapper.DeviceIndex>();
+        public static IEnumerable<CoolermasterSdkWrapper.DeviceIndex> SupportedKeyboardDevices => SupportedDevices.Where(d => d != CoolermasterSdkWrapper.DeviceIndex.DevMMouseL && d != CoolermasterSdkWrapper.DeviceIndex.DevMMouseS);
+        public static IEnumerable<CoolermasterSdkWrapper.DeviceIndex> SupportedMouseDevices => SupportedDevices.Except(SupportedKeyboardDevices);
+
+        private static CoolermasterSdkWrapper.KeyColor MapColour(Color color) => new CoolermasterSdkWrapper.KeyColor(color.R, color.G, color.B);
+
+        public CoolermasterLib()
+        {
+            _colorMatrix.KeyColor = new CoolermasterSdkWrapper.KeyColor[CoolermasterSdkWrapper.MaxLedRow, CoolermasterSdkWrapper.MaxLedColumn];
+
+
+
+            _updateTimer = new Timer((_) => ApplyKeyboardLighting(), null, Timeout.Infinite, Timeout.Infinite);
+        }
+
+        public void ApplyMapKeyLighting(string key, Color col, bool clear, [Optional] bool bypasswhitelist)
+        {
+            // clear is unused?
+            // whitelist appears to be a blacklist
+
+            // macro 5, 16, 17, 18???
+
+            if (!bypasswhitelist && FfxivHotbar.Keybindwhitelist.Contains(key))
+                return;
+
+            if (!KeyMappings.ContainsKey(key))
+            {
+                Debug.WriteLine($"Unknown key: '{key}'");
+                return;
+            }
+
+            _colorMatrix.KeyColor[KeyMappings[key][0], KeyMappings[key][1]] = MapColour(col);
+
+            ApplyKeyboardLightingSoon();
+        }
+
+        public void ApplyMapMouseLighting(string region, Color col, bool clear)
+        {
+        }
+
+        public void Flash1(Color burstcol, int speed, string[] region)
+        {
+        }
+
+        public void Flash2(Color burstcol, int speed, CancellationToken cts, string[] regions)
+        {
+        }
+
+        public void Flash3(Color burstcol, int speed, CancellationToken cts)
+        {
+        }
+
+        public void Flash4(Color burstcol, int speed, CancellationToken cts, string[] regions)
+        {
+        }
+
+        public bool InitializeSdk()
+        {
+            try
+            {
+                lock (_initLock)
+                {
+                    if (IsInitialized)
+                        return true;
+
+                    Write.WriteConsole(ConsoleTypes.Coolermaster, "Attempting to initializer Coolermaster support...");
+
+                    foreach (var supportedDevice in SupportedKeyboardDevices)
+                    {
+                        CoolermasterSdkWrapper.SetControlDevice(supportedDevice);
+                        if (CoolermasterSdkWrapper.IsDevicePlug())
+                        {
+                            Write.WriteConsole(ConsoleTypes.Coolermaster, $"Found a {supportedDevice} Coolermaster keyboard.");
+                            _keyboards.Add(supportedDevice);
+                        }
+                    }
+
+                    // TODO(devices): Coolermaster Mice
+
+                    if (_keyboards.Any())
+                    {
+                        IsInitialized = true;
+                        return true;
+                    }
+                    else
+                    {
+                        Write.WriteConsole(ConsoleTypes.Coolermaster, "Did not find any supported Coolermaster devices.");
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Write.WriteConsole(ConsoleTypes.Coolermaster, "Coolermaster SDK failed to load. Error: " + ex.Message);
+            }
+            return false;
+        }
+
+        public void ResetCoolermasterDevices(bool deviceKeyboard, bool deviceMouse, Color basecol)
+        {
+        }
+
+        public Task Ripple1(Color burstcol, int speed)
+        {
+            return new Task(() => Task.Delay(speed * 9).Wait());
+        }
+
+        public Task Ripple2(Color burstcol, int speed)
+        {
+            return new Task(() => Task.Delay(speed * 9).Wait());
+        }
+
+        public void SetLights(Color col)
+        {
+            foreach(var mapping in KeyMappings)
+            {
+                _colorMatrix.KeyColor[mapping.Value[0], mapping.Value[1]] = MapColour(col);
+            }
+
+            ApplyKeyboardLighting();
+        }
+
+        public void SetWave()
+        {
+        }
+
+        public void Shutdown()
+        {
+        }
+
+        public void StopEffects()
+        {
+        }
+
+        private void ApplyKeyboardLighting()
+        {
+            foreach(var keyboardDevice in _keyboards)
+            {
+                CoolermasterSdkWrapper.SetControlDevice(keyboardDevice);
+                CoolermasterSdkWrapper.SetAllLedColor(_colorMatrix);
+            }
+
+            _updateTimer.Change(Timeout.Infinite, Timeout.Infinite);
+        }
+
+        private void ApplyKeyboardLightingSoon()
+        {
+            _updateTimer.Change(TimeSpan.FromMilliseconds(50), Timeout.InfiniteTimeSpan);
+        }
+    }
+
+    public class OldCoolermasterLib : ICoolermasterSdk
+    {
         private static readonly ILogWrite Write = SimpleIoc.Default.GetInstance<ILogWrite>();
         private static bool _boot;
 
