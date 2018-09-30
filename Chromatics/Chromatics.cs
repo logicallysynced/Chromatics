35c8cdb43bb18ef7975fd2d3e10ca61d3528cbc3
Roxas Keyheart <technicaldaniel@gmail.com>
2018-09-30 11:34:38 +0930

    2.4.4 Release

--- Chromatics/Chromatics.cs
+++ Chromatics/Chromatics.cs
@@ -68,7 +68,7 @@ namespace Chromatics
         public bool CorsairSdk = false;
         public int CorsairSdkCalled = 0;
         private readonly string _currentVersionX = "2.4.4";
-        private readonly bool _debugmode = true;
+        private readonly bool _debugmode = false;
         public bool DeviceGridStartup = false;
         private bool blockACTVersion = false;
         private const int ACTVersionMatch = 1;
