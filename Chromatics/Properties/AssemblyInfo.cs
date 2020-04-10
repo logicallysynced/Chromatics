6edb15c95f54ab87edce496bf3524690e7ecabca
Roxas Keyheart <Roxas@Laptop>
2020-04-10 18:33:25 +0930

    Revert "Revert "2.5.6 Release""
    This reverts commit 7d9cca2f60b8abc5bc7f1657320eaec77b7ed60e.

--- Chromatics/Properties/AssemblyInfo.cs
+++ Chromatics/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Chromatics")]
-[assembly: AssemblyCopyright("Copyright © Roxas Keyheart 2019")]
+[assembly: AssemblyCopyright("Copyright © Roxas Keyheart 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.5.4.0")]
-[assembly: AssemblyFileVersion("2.5.4.0")]
+[assembly: AssemblyVersion("2.5.6.0")]
+[assembly: AssemblyFileVersion("2.5.6.0")]
