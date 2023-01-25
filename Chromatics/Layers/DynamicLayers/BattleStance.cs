a862b5db4a7ebd380392d57cd0d0fb1f390c4f0b
Danielle <logicallysynced@gmail.com>
2023-01-25 20:19:57 +1100

    Added Battle stance dynamic layer.

--- Chromatics/Layers/DynamicLayers/BattleStance.cs
+++ Chromatics/Layers/DynamicLayers/BattleStance.cs
@@ -1,6 +1,11 @@
-﻿using Chromatics.Interfaces;
+﻿using Chromatics.Core;
+using Chromatics.Extensions.RGB.NET;
+using Chromatics.Helpers;
+using Chromatics.Interfaces;
+using RGB.NET.Core;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,8 +16,84 @@ namespace Chromatics.Layers
     {
         public override void Process(IMappingLayer layer)
         {
-            //Reactive Weather Base Layer Implementation
-            throw new NotImplementedException();
+            //Do not apply if currently in Preview mode
+            if (MappingLayers.IsPreview()) return;
+
+            //Static Base Layer Implementation
+            var _colorPalette = RGBController.GetActivePalette();
+            var engaged_color = ColorHelper.ColorToRGBColor(_colorPalette.BattleEngaged.Color);
+            var empty_color = ColorHelper.ColorToRGBColor(_colorPalette.BattleNotEngaged.Color); //bleed layer
+            var _layergroups = RGBController.GetLiveLayerGroups();
+
+            //loop through all LED's and assign to device layer (Order of LEDs is not important for a highlight layer)
+            var surface = RGBController.GetLiveSurfaces();
+            var devices = surface.GetDevices(layer.deviceType);
+            var ledArray = devices.SelectMany(d => d).Where(led => layer.deviceLeds.Any(v => v.Value.Equals(led.Id))).ToArray();
+                       
+
+            // Add new PublicListLedGroup to _layergroups with updated leds and create a new instance of it
+            PublicListLedGroup updatedLayerGroup;
+
+            if (_layergroups.ContainsKey(layer.layerID))
+            {
+                updatedLayerGroup = _layergroups[layer.layerID].FirstOrDefault();
+
+                if (layer.requestUpdate)
+                {
+                    // Replace the existing leds
+                    updatedLayerGroup.RemoveLeds(updatedLayerGroup);
+                    updatedLayerGroup.AddLeds(ledArray);
+
+                }
+            } else {
+                updatedLayerGroup = new PublicListLedGroup(surface, ledArray)
+                {
+                    ZIndex = layer.zindex,
+                };
+
+                var lg = new PublicListLedGroup[] { updatedLayerGroup };
+                _layergroups.Add(layer.layerID, lg);
+                updatedLayerGroup.Detach();
+
+            }
+                        
+            if (!layer.Enabled)
+            {
+                updatedLayerGroup.Detach();
+                return;
+            }
+
+
+            //Process data from FFXIV
+            var _memoryHandler = GameController.GetGameData();
+            var brush = new SolidColorBrush(engaged_color);
+
+            if (_memoryHandler?.Reader != null && _memoryHandler.Reader.CanGetActors())
+            {
+                var getCurrentPlayer = _memoryHandler.Reader.GetCurrentPlayer();
+                var inCombat = getCurrentPlayer.Entity.InCombat;
+                
+                if (!inCombat)
+                {
+                    if (layer.allowBleed)
+                    {
+                        brush.Color = Color.Transparent;
+                    }
+                    else
+                    {
+                        brush.Color = empty_color;
+                    }
+                    
+                }
+
+            }
+                        
+
+            //Apply lighting
+            updatedLayerGroup.Brush = brush;
+            updatedLayerGroup.Attach(surface);
+            _init = true;
+            layer.requestUpdate = false;
         }
     }
 }
