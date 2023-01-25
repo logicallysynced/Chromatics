5791fd4c1d0148594385d0e9c409b27ac1a7b338
Danielle <logicallysynced@gmail.com>
2023-01-25 20:14:23 +1100

    Added Dynamic Job classes layer.

--- Chromatics/Layers/DynamicLayers/JobClass.cs
+++ Chromatics/Layers/DynamicLayers/JobClass.cs
@@ -1,4 +1,9 @@
-﻿using Chromatics.Interfaces;
+﻿using Chromatics.Core;
+using Chromatics.Extensions.RGB.NET;
+using Chromatics.Helpers;
+using Chromatics.Interfaces;
+using RGB.NET.Core;
+using Sharlayan.Core.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,8 +16,81 @@ namespace Chromatics.Layers
     {
         public override void Process(IMappingLayer layer)
         {
-            //Reactive Weather Base Layer Implementation
-            throw new NotImplementedException();
+            //Do not apply if currently in Preview mode
+            if (MappingLayers.IsPreview()) return;
+
+            //Static Base Layer Implementation
+            var _colorPalette = RGBController.GetActivePalette();
+            var highlight_col = Color.Transparent;
+            var _layergroups = RGBController.GetLiveLayerGroups();
+
+            //loop through all LED's and assign to device layer (Order of LEDs is not important for a highlight layer)
+            var surface = RGBController.GetLiveSurfaces();
+            var devices = surface.GetDevices(layer.deviceType);
+            var ledArray = devices.SelectMany(d => d).Where(led => layer.deviceLeds.Any(v => v.Value.Equals(led.Id))).ToArray();
+                       
+
+            // Add new PublicListLedGroup to _layergroups with updated leds and create a new instance of it
+            PublicListLedGroup updatedLayerGroup;
+
+            if (_layergroups.ContainsKey(layer.layerID))
+            {
+                updatedLayerGroup = _layergroups[layer.layerID].FirstOrDefault();
+
+                if (layer.requestUpdate)
+                {
+                    // Replace the existing leds
+                    updatedLayerGroup.RemoveLeds(updatedLayerGroup);
+                    updatedLayerGroup.AddLeds(ledArray);
+
+                }
+            } else {
+                updatedLayerGroup = new PublicListLedGroup(surface, ledArray)
+                {
+                    ZIndex = layer.zindex,
+                };
+
+                var lg = new PublicListLedGroup[] { updatedLayerGroup };
+                _layergroups.Add(layer.layerID, lg);
+                updatedLayerGroup.Detach();
+
+            }
+                        
+            if (!layer.Enabled)
+            {
+                updatedLayerGroup.Detach();
+                return;
+            }
+
+
+            //Process data from FFXIV
+            var _memoryHandler = GameController.GetGameData();
+
+            if (_memoryHandler?.Reader != null && _memoryHandler.Reader.CanGetActors())
+            {
+                var getCurrentPlayer = _memoryHandler.Reader.GetCurrentPlayer();
+                var currentJob = getCurrentPlayer.Entity.Job;
+
+                highlight_col = GameHelper.GetJobClassColor(currentJob, _colorPalette, true);
+                
+                foreach (var led in updatedLayerGroup)
+                {
+                    if (led.Color != highlight_col)
+                    {
+                        led.Color = highlight_col;
+                    }
+
+                }
+                
+            }
+                        
+
+            //Apply lighting
+            var brush = new SolidColorBrush(highlight_col);
+            updatedLayerGroup.Brush = brush;
+            updatedLayerGroup.Attach(surface);
+            _init = true;
+            layer.requestUpdate = false;
         }
     }
 }
