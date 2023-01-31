6360f2b78f17afe87c44fc5dcf6b7b802adbe826
Danielle <logicallysynced@gmail.com>
2023-02-01 00:51:08 +1100

    Started some initial testing of job gauges. Re-built Sharlayan with some fixes to 6.0 job gauges.

--- Chromatics/Layers/DynamicLayers/JobGaugeA.cs
+++ Chromatics/Layers/DynamicLayers/JobGaugeA.cs
@@ -1,6 +1,9 @@
-﻿using Chromatics.Interfaces;
+﻿using Chromatics.Core;
+using Chromatics.Helpers;
+using Chromatics.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +15,20 @@ namespace Chromatics.Layers
         public override void Process(IMappingLayer layer)
         {
             //Job Gauge A Dynamic Layer Implementation
-            //throw new NotImplementedException();
+            if (MappingLayers.IsPreview()) return;
+
+            var _memoryHandler = GameController.GetGameData();
+
+            if (_memoryHandler?.Reader != null && _memoryHandler.Reader.CanGetJobResources())
+            {
+                var getJobResources = _memoryHandler.Reader.GetJobResources();
+                if (getJobResources.JobResourcesContainer == null) return;
+
+                Debug.WriteLine($"Song Timer: {getJobResources.JobResourcesContainer.Bard.Timer}");
+                Debug.WriteLine($"Stacks: {getJobResources.JobResourcesContainer.Bard.Repertoire}");
+                Debug.WriteLine($"Voice: {getJobResources.JobResourcesContainer.Bard.SoulVoice}");
+                Debug.WriteLine($"Song: {getJobResources.JobResourcesContainer.Bard.ActiveSong}");
+            }
         }
     }
 }
