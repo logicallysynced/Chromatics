a14728fa62e3ef314cd3d23b8fae3d3d54cd56e6
Danielle <logicallysynced@gmail.com>
2023-02-06 15:17:49 +1100

    Fixed bug where job class base and highlight colours were inverted.

--- Chromatics/Helpers/GameHelper.cs
+++ Chromatics/Helpers/GameHelper.cs
@@ -49,202 +49,202 @@ namespace Chromatics.Helpers
             switch (job)
             {
                 case Actor.Job.CUL:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobCULBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobCULHighlight.Color);
                 case Actor.Job.GLD:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobPLDBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobPLDHighlight.Color);
                 case Actor.Job.PGL:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobMNKBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobMNKHighlight.Color);
                 case Actor.Job.MRD:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobWARBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobWARHighlight.Color);
                 case Actor.Job.LNC:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobDRGBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobDRGHighlight.Color);
                 case Actor.Job.ARC:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobBRDBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobBRDHighlight.Color);
                 case Actor.Job.CNJ:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobWHMBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobWHMHighlight.Color);
                 case Actor.Job.THM:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobBLMBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobBLMHighlight.Color);
                 case Actor.Job.CPT:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobCPTBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobCPTHighlight.Color);
                 case Actor.Job.BSM:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobBLMBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobBLMHighlight.Color);
                 case Actor.Job.ARM:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobARMBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobARMHighlight.Color);
                 case Actor.Job.GSM:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobGSMBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobGSMHighlight.Color);
                 case Actor.Job.LTW:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobLTWBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobLTWHighlight.Color);
                 case Actor.Job.WVR:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobWVRBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobWVRHighlight.Color);
                 case Actor.Job.ALC:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobALCBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobALCHighlight.Color);
                 case Actor.Job.MIN:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobMINBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobMINHighlight.Color);
                 case Actor.Job.BTN:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobBTNBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobBTNHighlight.Color);
                 case Actor.Job.FSH:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobFSHBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobFSHHighlight.Color);
                 case Actor.Job.PLD:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobPLDBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobPLDHighlight.Color);
                 case Actor.Job.MNK:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobMNKBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobMNKHighlight.Color);
                 case Actor.Job.WAR:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobWARBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobWARHighlight.Color);
                 case Actor.Job.DRG:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobDRGBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobDRGHighlight.Color);
                 case Actor.Job.BRD:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobBRDBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobBRDHighlight.Color);
                 case Actor.Job.WHM:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobWHMBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobWHMHighlight.Color);
                 case Actor.Job.BLM:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobBLMBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobBLMHighlight.Color);
                 case Actor.Job.ACN:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobWHMBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobWHMHighlight.Color);
                 case Actor.Job.SMN:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobSMNBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobSMNHighlight.Color);
                 case Actor.Job.SCH:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobSCHBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobSCHHighlight.Color);
                 case Actor.Job.ROG:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobNINBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobNINHighlight.Color);
                 case Actor.Job.NIN:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobNINBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobNINHighlight.Color);
                 case Actor.Job.MCH:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobMCHBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobMCHHighlight.Color);
                 case Actor.Job.DRK:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobDRKBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobDRKHighlight.Color);
                 case Actor.Job.AST:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobASTBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobASTHighlight.Color);
                 case Actor.Job.SAM:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobSAMBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobSAMHighlight.Color);
                 case Actor.Job.RDM:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobRDMBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobRDMHighlight.Color);
                 case Actor.Job.BLU:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobBLUBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobBLUHighlight.Color);
                 case Actor.Job.GNB:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobGNBBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobGNBHighlight.Color);
                 case Actor.Job.DNC:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobDNCBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobDNCHighlight.Color);
                 case Actor.Job.RPR:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobRPRBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobRPRHighlight.Color);
                 case Actor.Job.SGE:
-                    if (highlight)
+                    if (!highlight)
                         return ColorHelper.ColorToRGBColor(colorPalette.JobSGEBase.Color);
                     else
                         return ColorHelper.ColorToRGBColor(colorPalette.JobSGEHighlight.Color);
